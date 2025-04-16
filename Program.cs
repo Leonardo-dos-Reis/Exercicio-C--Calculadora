6390bc7453b265188ad23c2fd8daee969ea8e937
L Reis <leo.reis1802@hotmail.com>
2025-04-16 14:28:54 -0300

    adição do método 'SOMA'

--- Program.cs
+++ Program.cs
@@ -1,2 +1,37 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace CalculadoraCSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Clear();
+            Soma();
+        }
+
+        static void Menu()
+        {
+            Console.Clear();
+            Console.WriteLine("O que deseja realizar?");
+        }
+
+        static void Soma()
+        {
+            Console.Clear();
+            Console.WriteLine("--- SOMA ---");
+            Console.WriteLine("");
+
+            Console.WriteLine("Digite o primeiro valor : ");
+            float v1 = float.Parse(Console.ReadLine());
+            Console.WriteLine("");
+
+            Console.WriteLine("Digite o segundo valor : ");
+            float v2 = float.Parse(Console.ReadLine());
+            float resultado = v1 + v2;
+            Console.Clear();
+            
+            Console.WriteLine($"O resultado da soma dos valores é : {resultado}");
+        }
+    }
+}
